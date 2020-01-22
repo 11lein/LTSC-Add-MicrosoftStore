3511d76a922afec75937bf5e126d52ef21b4d453
Ronny Elflein <ronny.elflein@aoe.com>
2020-01-22 15:50:02 +0100

    newer Version removed  XBox

--- Add-Store.cmd
+++ Add-Store.cmd
@@ -1,20 +1,28 @@
 @echo off
+set "SysPath=%Windir%\System32"
+if exist "%Windir%\Sysnative\reg.exe" (set "SysPath=%Windir%\Sysnative")
+set "Path=%SysPath%;%Windir%;%SysPath%\Wbem;%SysPath%\WindowsPowerShell\v1.0\"
 for /f "tokens=6 delims=[]. " %%G in ('ver') do if %%G lss 16299 goto :version
-%windir%\system32\reg.exe query "HKU\S-1-5-19" 1>nul 2>nul || goto :uac
-setlocal enableextensions
-if /i "%PROCESSOR_ARCHITECTURE%" equ "AMD64" (set "arch=x64") else (set "arch=x86")
-cd /d "%~dp0"
-
+fsutil dirty query %systemdrive% 1>nul 2>nul || goto :uac
+set "arch=x64"
+if /i %PROCESSOR_ARCHITECTURE%==x86 (if not defined PROCESSOR_ARCHITEW6432 set "arch=x86")
+pushd "%~dp0"
 if not exist "*WindowsStore*.appxbundle" goto :nofiles
 if not exist "*WindowsStore*.xml" goto :nofiles
 
 for /f %%i in ('dir /b *WindowsStore*.appxbundle 2^>nul') do set "Store=%%i"
-for /f %%i in ('dir /b *NET.Native.Framework*1.6*.appx 2^>nul ^| find /i "x64"') do set "Framework6X64=%%i"
-for /f %%i in ('dir /b *NET.Native.Framework*1.6*.appx 2^>nul ^| find /i "x86"') do set "Framework6X86=%%i"
-for /f %%i in ('dir /b *NET.Native.Runtime*1.6*.appx 2^>nul ^| find /i "x64"') do set "Runtime6X64=%%i"
-for /f %%i in ('dir /b *NET.Native.Runtime*1.6*.appx 2^>nul ^| find /i "x86"') do set "Runtime6X86=%%i"
-for /f %%i in ('dir /b *VCLibs*140*.appx 2^>nul ^| find /i "x64"') do set "VCLibsX64=%%i"
-for /f %%i in ('dir /b *VCLibs*140*.appx 2^>nul ^| find /i "x86"') do set "VCLibsX86=%%i"
+for /f %%i in ('dir /b *NET.Native.Framework*1.7*.appx 2^>nul ^| find /i "x64"') do set "FK7X64=%%i"
+for /f %%i in ('dir /b *NET.Native.Framework*1.7*.appx 2^>nul ^| find /i "x86"') do set "FK7X86=%%i"
+for /f %%i in ('dir /b *NET.Native.Runtime*1.7*.appx 2^>nul ^| find /i "x64"') do set "RT7X64=%%i"
+for /f %%i in ('dir /b *NET.Native.Runtime*1.7*.appx 2^>nul ^| find /i "x86"') do set "RT7X86=%%i"
+for /f %%i in ('dir /b *VCLibs*140.00_*.appx 2^>nul ^| find /i "x64"') do set "VCPX64=%%i"
+for /f %%i in ('dir /b *VCLibs*140.00_*.appx 2^>nul ^| find /i "x86"') do set "VCPX86=%%i"
+for /f %%i in ('dir /b *NET.Native.Framework*2.2*.appx 2^>nul ^| find /i "x64"') do set "FK2X64=%%i"
+for /f %%i in ('dir /b *NET.Native.Framework*2.2*.appx 2^>nul ^| find /i "x86"') do set "FK2X86=%%i"
+for /f %%i in ('dir /b *NET.Native.Runtime*2.2*.appx 2^>nul ^| find /i "x64"') do set "RT2X64=%%i"
+for /f %%i in ('dir /b *NET.Native.Runtime*2.2*.appx 2^>nul ^| find /i "x86"') do set "RT2X86=%%i"
+for /f %%i in ('dir /b *VCLibs*140.00.UWPDesktop*.appx 2^>nul ^| find /i "x64"') do set "VCDX64=%%i"
+for /f %%i in ('dir /b *VCLibs*140.00.UWPDesktop*.appx 2^>nul ^| find /i "x86"') do set "VCDX86=%%i"
 
 if exist "*StorePurchaseApp*.appxbundle" if exist "*StorePurchaseApp*.xml" (
 for /f %%i in ('dir /b *StorePurchaseApp*.appxbundle 2^>nul') do set "PurchaseApp=%%i"
@@ -22,20 +30,15 @@ for /f %%i in ('dir /b *StorePurchaseApp*.appxbundle 2^>nul') do set "PurchaseAp
 if exist "*DesktopAppInstaller*.appxbundle" if exist "*DesktopAppInstaller*.xml" (
 for /f %%i in ('dir /b *DesktopAppInstaller*.appxbundle 2^>nul') do set "AppInstaller=%%i"
 )
-if exist "*XboxIdentityProvider*.appxbundle" if exist "*XboxIdentityProvider*.xml" (
-for /f %%i in ('dir /b *XboxIdentityProvider*.appxbundle 2^>nul') do set "XboxIdentity=%%i"
-)
 
 if /i %arch%==x64 (
-set "DepStore=%VCLibsX64%,%VCLibsX86%,%Framework6X64%,%Framework6X86%,%Runtime6X64%,%Runtime6X86%"
-set "DepPurchase=%VCLibsX64%,%VCLibsX86%,%Framework6X64%,%Framework6X86%,%Runtime6X64%,%Runtime6X86%"
-set "DepXbox=%VCLibsX64%,%VCLibsX86%,%Framework6X64%,%Framework6X86%,%Runtime6X64%,%Runtime6X86%"
-set "DepInstaller=%VCLibsX64%,%VCLibsX86%"
+set "DepStore=%FK7X64%,%FK7X86%,%RT7X64%,%RT7X86%,%VCPX64%,%VCPX86%"
+set "DepPurchase=%FK7X64%,%FK7X86%,%RT7X64%,%RT7X86%,%VCPX64%,%VCPX86%"
+set "DepInstaller=%VCDX64%,%VCDX86%,%VCPX64%,%VCPX86%"
 ) else (
-set "DepStore=%VCLibsX86%,%Framework6X86%,%Runtime6X86%"
-set "DepPurchase=%VCLibsX86%,%Framework6X86%,%Runtime6X86%"
-set "DepXbox=%VCLibsX86%,%Framework6X86%,%Runtime6X86%"
-set "DepInstaller=%VCLibsX86%"
+set "DepStore=%FK7X86%,%RT7X86%,%VCPX86%"
+set "DepPurchase=%FK7X86%,%RT7X86%,%VCPX86%"
+set "DepInstaller=%VCDX86%,%VCPX86%"
 )
 
 for %%i in (%DepStore%) do (
@@ -64,6 +67,7 @@ echo.
 1>nul 2>nul %PScommand% Add-AppxProvisionedPackage -Online -PackagePath %PurchaseApp% -DependencyPackagePath %DepPurchase% -LicensePath Microsoft.StorePurchaseApp_8wekyb3d8bbwe.xml
 %PScommand% Add-AppxPackage -Path %PurchaseApp%
 )
+
 if defined AppInstaller (
 echo.
 echo ============================================================
@@ -71,18 +75,11 @@ echo Adding App Installer
 echo ============================================================
 echo.
 1>nul 2>nul %PScommand% Add-AppxProvisionedPackage -Online -PackagePath %AppInstaller% -DependencyPackagePath %DepInstaller% -LicensePath Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.xml
+for %%i in (%DepInstaller%) do (
+%PScommand% Add-AppxPackage -Path %%i
+)
 %PScommand% Add-AppxPackage -Path %AppInstaller%
 )
-if defined XboxIdentity (
-echo.
-echo ============================================================
-echo Adding Xbox Identity Provider
-echo ============================================================
-echo.
-1>nul 2>nul %PScommand% Add-AppxProvisionedPackage -Online -PackagePath %XboxIdentity% -DependencyPackagePath %DepXbox% -LicensePath Microsoft.XboxIdentityProvider_8wekyb3d8bbwe.xml
-%PScommand% Add-AppxPackage -Path %XboxIdentity%
-)
-goto :fin
 
 :uac
 echo.
@@ -118,6 +115,7 @@ pause >nul
 exit
 
 :fin
+popd
 echo.
 echo ============================================================
 echo Done
